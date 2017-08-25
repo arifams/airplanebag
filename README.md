62192b544372cb526aad0d44a39f0b7f815609d8
arifpackage <akurniawan@parcelinternational.com>
2017-08-25 16:45:44 +0200

    fix header for desktop view

---
+++ README.md
@@ -0,0 +1,7 @@
+# Smart Luggage
+
+#### App to help passangers send their luggage to airport or to direct destination
+
+### Configuration
+- Front-end: Bootstrap, Custom CSS, JQuery plugins, Vanilla JS, HTML5
+- Back-end: (Cake) PHP
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 // Empty JS for own code to be here
+$('#navbar_sso .panel-collapse').on('shown.bs.collapse', function () {
+    $(this).prev().find(".glyphicon").removeClass("glyphicon-chevron-right").addClass("glyphicon-chevron-down");
+});
 
+
+/* isNumberKey
+    Only allows NUMBERS to be keyed into a text field.
+    @environment ALL
+    @param evt - The specified EVENT that happens on the element.
+    @return True if number, false otherwise.
+*/
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    // Added to allow decimal, period, or delete
+    if (charCode == 110 || charCode == 190 || charCode == 46) 
+        return true;
+    
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) 
+        return false;
+    
+    return true;
+} 
 
 
 // to check pickup date
